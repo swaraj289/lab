54b857516db448b81c00d00ec620ed0a4914e7d7
Swaraj Mahindrakar <smahindrakar289@gmail.com>
2022-02-15 19:39:37 +0530

    Add files via upload

---
+++ program7a/app.js
@@ -0,0 +1,7 @@
+request=require('request');
+const url="http://api.weatherstack.com/current?access_key=01eddd3e3f5bd04f222de5dff404880a&query=New%20York&units=f";
+request({url:url},(err,response)=>{
+const data=JSON.parse(response.body);
+console.log("The current temperature is ",data.current.temperature);
+console.log("Country is",data.location.country);
+});
